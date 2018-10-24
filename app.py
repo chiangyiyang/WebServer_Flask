7655ee9ddf76bf35b775accc20bba2e94f6b54a9
Yiyang <chiangyiyang@gmail.com>
2018-10-24 09:01:54 +0800

    加入HTML 2

--- app.py
+++ app.py
@@ -5,9 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    page = '''
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph....</p>
+
+</body>
+</html>
+  '''
+    return page
 
 
 app.run(
-  host='0.0.0.0',   # 設定對外服務IP
-  port=8080)        # 設定對外服務Port
+    host='0.0.0.0',   # 設定對外服務IP
+    port=8080)        # 設定對外服務Port
